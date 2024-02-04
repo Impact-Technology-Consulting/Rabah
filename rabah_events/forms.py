62be2a88a24f9f07a123c619dff216cee6a03dbc
Favour Afenikhena <codertjay@gmail.com>
2024-02-04 16:57:37 +0100

    added ui for mark attendab=nce

--- rabah_events/forms.py
+++ rabah_events/forms.py
@@ -71,3 +71,46 @@ class EventCreateForm(forms.ModelForm):
         if commit:
             instance.save()
         return instance
+
+
+class EventUpdateForm(forms.ModelForm):
+    start_date = forms.DateTimeField(
+        widget=DateTimeInput(attrs={'type': 'datetime-local'}),
+    )
+    end_date = forms.DateTimeField(
+        widget=DateTimeInput(attrs={'type': 'datetime-local'}),
+    )
+
+    class Meta:
+        model = Event
+        fields = [
+            "name",
+            "image",
+            "description",
+            "start_date",
+            "end_date",
+        ]
+
+    def __init__(self, organisation_id, *args, **kwargs):
+        super(EventUpdateForm, self).__init__(*args, **kwargs)
+        self.fields['organisation_id'] = forms.CharField(widget=forms.HiddenInput(), initial=organisation_id)
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        start_date = cleaned_data.get("start_date")
+        end_date = cleaned_data.get("end_date")
+
+        # Validate that start_date is before end_date
+        if start_date and end_date and start_date >= end_date:
+            self.add_error("end_date", "End date must be after start date.")
+
+        return cleaned_data
+
+    def save(self, commit=True):
+        instance = super(EventUpdateForm, self).save(commit=False)
+        instance.organisation_id = self.cleaned_data['organisation_id']
+        instance.parent_event = instance
+        if commit:
+            instance.save()
+        return instance
--- rabah_events/views.py
+++ rabah_events/views.py
@@ -5,8 +5,9 @@ from django.http import HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
 from django.utils import timezone
 from django.views import View
+from django.views.generic import ListView
 
-from rabah_events.forms import EventCreateForm
+from rabah_events.forms import EventCreateForm, EventUpdateForm
 from rabah_events.models import Event, EventMember, MemberAttendance
 from rabah_events.tasks import create_event_for_repeat_count, create_event_for_until_date
 from rabah_members.models import Member
@@ -15,6 +16,9 @@ from users.mixin import AuthAndAdminOrganizationMemberMixin
 
 
 class EventView(AuthAndAdminOrganizationMemberMixin, View):
+    """
+    this is the calendar page where users get access to view events using the calendar
+    """
 
     def get(self, request):
         organisation_id = self.organisation_id
@@ -53,9 +57,57 @@ class EventView(AuthAndAdminOrganizationMemberMixin, View):
             return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 
+class EventUpdateView(AuthAndAdminOrganizationMemberMixin, View):
+    """
+    this is used to update an event
+    """
+
+    def get(self, request, event_id):
+        event = Event.objects.filter(id=event_id, organisation_id=self.organisation_id).first()
+        if not event:
+            messages.error(request, "Event with this id does not exist in this organisation")
+            return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+        form = EventUpdateForm(self.organisation_id, instance=event)
+        context = {
+            "form": form,
+            "event": event
+        }
+        return render(request, "dashboard/event_update.html", context)
+
+    def post(self, request, event_id):
+        event = Event.objects.filter(id=event_id, organisation_id=self.organisation_id).first()
+        if not event:
+            messages.error(request, "Event with this id does not exist in this organisation")
+            return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+        form = EventUpdateForm(self.organisation_id, data=request.POST, files=request.FILES, instance=event)
+        if form.is_valid():
+            event = form.save()
+            messages.success(request, "Event updated successfully")
+            return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+        else:
+            for error in form.errors:
+                messages.warning(request, f"{error}: {form.errors[error][0]}")
+            return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+
+class EventListView(AuthAndAdminOrganizationMemberMixin, ListView):
+    """
+    this is used to list all the events available, and it only shows from future events or current events
+    """
+    queryset = Event.objects.all()
+    template_name = "dashboard/event_list.html"
+    context_object_name = "events"
+    paginate_by = 20
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(organisation_id=self.organisation_id, start_date__gte=timezone.now())
+
+
 class EventCalendarView(AuthAndAdminOrganizationMemberMixin, View):
     """
-    this is used to get all the calendar info on the events available
+    this is used to get all the calendar info api  on the events available
     """
 
     def get(self, request):
@@ -117,9 +169,14 @@ class MarkAttendancePageView(AuthAndAdminOrganizationMemberMixin, View):
             # Exclude members with the list of IDs
             members = members.exclude(id__in=present_member_ids)
 
+        present_count = member_attendance.filter(status="PRESENT").count()
+        absent_count = Member.objects.filter(organisation_id=self.organisation_id).count() - present_count
+
         context = {
             "members": members,
-            "event": event
+            "event": event,
+            "present_count":present_count,
+            "absent_count":absent_count
         }
         return render(request, "dashboard/mark_attendance.html", context)
 
@@ -155,10 +212,11 @@ class MarkMultipleMemberAttendanceView(AuthAndAdminOrganizationMemberMixin, View
                                                                     event=event)
             if member_id.get("is_checked"):
                 member_attendance.status = "PRESENT"
+                member_attendance.save()
+                event_member.members_attendance.add(member_attendance)
+                return JsonResponse({"message": "Attendance marked successfully for PRESENT"}, status=200)
             else:
                 member_attendance.status = "ABSENT"
-
-            member_attendance.save()
-
-            event_member.members_attendance.add(member_attendance)
-        return JsonResponse({"message": "Attendance marked successfully"}, status=200)
+                member_attendance.save()
+                event_member.members_attendance.add(member_attendance)
+                return JsonResponse({"message": "Attendance marked successfully for ABSENT"}, status=200)
