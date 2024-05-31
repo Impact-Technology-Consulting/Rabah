7d1a3dd9061272265121ade1a6723a3272d27b27
codertjay <codertjay@gmail.com>
2024-05-31 11:56:36 +0100

    removed check

--- Rabah/settings/local.py
+++ Rabah/settings/local.py
@@ -8,28 +8,28 @@ DEBUG = True
 ALLOWED_HOSTS = ["*"]
 SECRET_KEY = config("SECRET_KEY")
 
-# #  Removed the production database from here.
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
+#  Removed the production database from here.
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": config("POSTGRES_DB", default=""),
-        "USER": config("POSTGRES_USER", default=""),
-        "PASSWORD": config("POSTGRES_PASSWORD", default=""),
-        "HOST": config("POSTGRES_HOST", default=""),
-        "PORT": config("POSTGRES_PORT", default=""),
-    },
-    "secondary": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    },
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql_psycopg2",
+#         "NAME": config("POSTGRES_DB", default=""),
+#         "USER": config("POSTGRES_USER", default=""),
+#         "PASSWORD": config("POSTGRES_PASSWORD", default=""),
+#         "HOST": config("POSTGRES_HOST", default=""),
+#         "PORT": config("POSTGRES_PORT", default=""),
+#     },
+#     "secondary": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     },
+# }
 
 STRIPE_PUBLIC_KEY = config('STRIPE_TEST_PUBLIC_KEY')
 STRIPE_SECRET_KEY = config('STRIPE_TEST_SECRET_KEY')
